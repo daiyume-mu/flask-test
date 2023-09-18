1b933e09afc05b5e813b349e7a7c54d05bcabc48
daiuyme-mu <daiyume11@gmail.com>
2023-09-18 17:35:53 +0900

    リファクタリング

--- controllers/todo_controller.py
+++ controllers/todo_controller.py
@@ -3,18 +3,19 @@ from services import todo_service
 
 todo_blueprint = Blueprint('todo', __name__)
 
-@todo_blueprint.route('/', methods=['GET', 'POST'])
+@todo_blueprint.route('/')
 def index():
-    if request.method == 'GET':
-        posts = todo_service.get_all_posts()
-        return render_template('index.html', posts=posts)
-    else:
-        title = request.form.get('title')
-        detail = request.form.get('detail')
-        due = request.form.get('due')
-        todo_service.create_post(title, detail, due)
-        return redirect('/')
+    posts = todo_service.get_all_posts()
+    return render_template('index.html', posts=posts)
 
+@todo_blueprint.route('/store')
+def store():
+    title = request.form.get('title')
+    detail = request.form.get('detail')
+    due = request.form.get('due')
+    todo_service.create_post(title, detail, due)
+    return redirect('/')
+    
 @todo_blueprint.route('/create')
 def create():
     return render_template('create.html')
@@ -29,19 +30,23 @@ def delete(id):
     todo_service.delete_post(id)
     return redirect('/')
 
-@todo_blueprint.route('/update/<int:id>', methods=['GET', 'POST'])
+@todo_blueprint.route('/update/<int:id>')
 def update(id):
-    if request.method == 'GET':
-        post = todo_service.get_post_by_id(id)
-        return render_template('update.html', post=post)
-    else:
-        title = request.form.get('title')
-        detail = request.form.get('detail')
-        due = request.form.get('due')
-        
-        if title and detail and due:  # Make sure all variables have values
-            todo_service.update_post(id, title, detail, due)  # Pass all required arguments here
-            return redirect('/')
-        else:
-            return "Error: Missing required fields", 400
+    
+    title = request.form.get('title')
+    detail = request.form.get('detail')
+    due = request.form.get('due')
+    
+    #早期リターンとは何が存在してれば、処理を走らせるって書くのではなく、何が存在してなかったら処理を終わらせるというコードを先に書くこと。これにより、ちゃんとしてた時に走るコードが長ければ長いほどみやすくなる。
+    if not title and detail and due:  # Make sure all variables have values
+        return "Error: Missing required fields", 400
+    try:
+        todo_service.update_post(id, title, detail, due)  # Pass all required arguments here
+        return redirect('/')
+    except:
+        return "Error: Something went wrong", 500
 
+@todo_blueprint.route('/edit/<int:id>')
+def edit(id):
+    post = todo_service.get_post_by_id(id)
+    return render_template('update.html', post=post)
