9b8d0fddc919440651dd9be08bdbdd30c71d921f
daiuyme-mu <daiyume11@gmail.com>
2023-09-20 15:55:56 +0900

    バグ修正

--- models/post.py
+++ models/post.py
@@ -8,3 +8,12 @@ class Post(db.Model):
     detail = db.Column(db.String(100))
     due = db.Column(db.DateTime, nullable=False)
 
+    comments = db.relationship('CommentDate', back_populates='post')
+
+class CommentDate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    comment = db.Column(db.String(500))
+    comment_timestamp = db.Column(db.DateTime, nullable=False)
+
+    post = db.relationship('Post', back_populates='comments')
