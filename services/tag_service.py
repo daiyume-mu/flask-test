4370951bf617b95549fb5c4ec198160090794f89
daiuyme-mu <daiyume11@gmail.com>
2023-09-29 17:36:50 +0900

    コメントアウト部分の削除

--- services/tag_service.py
+++ services/tag_service.py
@@ -1,28 +1,6 @@
 from models.tag import Tag, db
 from models.post import Post,post_tag, db
 from sqlalchemy import desc
-
-"""def create_tag(post, tag_name):
-    existing_tag = Tag.query.filter_by(tag_name=tag_name).first()
-    if not existing_tag is None:
-        post.tags.append(existing_tag)
-    else:
-        new_tag = Tag(tag_name=tag_name)
-        db.session.add(new_tag)
-        post.tags.append(new_tag)
-        
-    db.session.commit()
-
-def update_tag(post, tag_name):
-    existing_tag = Tag.query.filter_by(tag_name=tag_name).first()
-    if existing_tag is not None:
-        post.tags.append(existing_tag)
-    else:
-        new_tag = Tag(tag_name=tag_name)
-        db.session.add(new_tag)
-        post.tags.append(new_tag)
-    
-    db.session.commit()"""
     
 def get_posts_by_tag_id(tag_id):
     posts = Post.query.options(db.joinedload(Post.tags)).order_by((Post.due)).all()
